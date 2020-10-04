6d991562314bc6e7adad7fbac1d070faba1f484b
Alon Ben Harosh <alonbh5@gmail.com>
2020-10-05 01:49:08 +0300

    PreOrganized Web Module

---
+++ SDMWebApp/src/test.java
@@ -0,0 +1,17 @@
+import course.java.sdm.engine.SuperDuperMarketSystem;
+
+public class test {
+
+    public void foo () {
+        SuperDuperMarketSystem gaga = new SuperDuperMarketSystem();
+        System.out.println(gaga);
+    }
+
+    public static void main(String[] args) {
+        test sdadsa = new test();
+        sdadsa.foo();
+    }
+}
+
+//todo - add Gson like in video minute 6
+//todo delete this test...
